c033fc66d509d527edc5143ff6e4ce97221666ce
Pfizz <pfiste80@gmail.com>
2021-07-25 19:24:11 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // javascript for challenge 5 //
 
-//indentify and grab current day id //
-
-
-//var currentDay = document.querySelector("currentDay");
 
 var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(currentTime);
-console.log(currentTime);
+
+// adds input field to each time block div
+function editCalendar() {
+    var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+    .addClass("form-control-sm")
+    .val(text);
+
+    $(this).find("p").replaceWith(textInput);
+
+    textInput.trigger("focus");
+};
+
+//create function to save data to localstorage
+
+function saveCalendar() {
+ console.log("placeholder");
+};
+
+//functions calls
+$("li #cal-enter").on("click", editCalendar)
+$("li .cal-enter").on("click", editCalendar)
